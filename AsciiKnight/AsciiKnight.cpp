bf3558e1e9e50d1536536383118af7a2797e3642
antihackerbg <antihackerbg@DESKTOP-QHMUGPQ>
2026-01-15 03:46:06 +0200

    Add project files.

---
+++ AsciiKnight/AsciiKnight.cpp
@@ -0,0 +1,29 @@
+/**
+*
+* Solution to course project # 10
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Alexander Basamakov
+* @idnumber 0MI0600579
+* @compiler VC
+*
+*
+*
+*/
+
+#include <iostream>
+#include <ctime>     
+#include <conio.h>   
+#include <windows.h> 
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    
+    return 0;
+}
